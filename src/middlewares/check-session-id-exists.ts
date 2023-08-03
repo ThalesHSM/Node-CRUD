96878ed7f7d76aaffe643711338f34b8dbccbed9
ThalesHSM <thsm271@gmail.com>
2023-08-03 20:21:25 -0300

    Node CRUD

---
+++ src/middlewares/check-session-id-exists.ts
@@ -0,0 +1,10 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+
+export async function checkSessionidExists (request: FastifyRequest, reply: FastifyReply) {
+  const sessionId = request.cookies.sessionId
+  if (!sessionId) {
+    return reply.status(401).send({
+      error: 'Unauthorized',
+    })
+  }
+}
